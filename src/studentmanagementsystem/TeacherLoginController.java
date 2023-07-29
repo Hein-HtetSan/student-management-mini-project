2f23327d0930b2dba5bb08e0a9c96bf014770559
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-07-29 18:04:47 +0630

    teacher login controller

--- src/studentmanagementsystem/TeacherLoginController.java
+++ src/studentmanagementsystem/TeacherLoginController.java
@@ -4,9 +4,23 @@
  */
 package studentmanagementsystem;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +29,13 @@ import javafx.fxml.Initializable;
  */
 public class TeacherLoginController implements Initializable {
 
+    @FXML
+    private TextField txtPid;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private Label lblStatus;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +43,45 @@ public class TeacherLoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void teacherRegister(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("TeacherRegister.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void onLogin(ActionEvent event) throws ClassNotFoundException, SQLException, IOException {
+        String pid = txtPid.getText();
+        String password = txtPassword.getText();
+        if(pid.isEmpty()){
+            lblStatus.setText("Pid cannot be blank");
+        }else if(password.isEmpty()){
+            lblStatus.setText("Password cannot be blank");
+        }else{
+            Connection con = DBConnection.getConnection();
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM teacher WHERE pid = ? and password = ?");
+            stmt.setString(1, pid);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+                lblStatus.setText("Login Success!");
+                txtPid.setText("");
+                txtPassword.setText("");
+                
+                Parent root = FXMLLoader.load(getClass().getResource("Attendance.fxml"));
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.show();
+                
+            }else {
+                lblStatus.setText("Login Failed!");
+            }
+        }
+    }
     
 }
