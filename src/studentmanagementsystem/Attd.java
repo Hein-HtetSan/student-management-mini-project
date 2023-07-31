d08e9439cf1f848db29b01d7b2f70a11b4c765ef
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-07-31 15:39:22 +0630

    attendance subclass

---
+++ src/studentmanagementsystem/Attd.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentmanagementsystem;
+
+/**
+ *
+ * @author acer
+ */
+public class Attd {
+
+    /**
+     * @return the stime
+     */
+    public String getStime() {
+        return stime;
+    }
+
+    /**
+     * @param stime the stime to set
+     */
+    public void setStime(String stime) {
+        this.stime = stime;
+    }
+
+    /**
+     * @return the etime
+     */
+    public String getEtime() {
+        return etime;
+    }
+
+    /**
+     * @param etime the etime to set
+     */
+    public void setEtime(String etime) {
+        this.etime = etime;
+    }
+
+    /**
+     * @return the subject
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * @param subject the subject to set
+     */
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    /**
+     * @return the stdname
+     */
+    public String getStdname() {
+        return stdname;
+    }
+
+    /**
+     * @param stdname the stdname to set
+     */
+    public void setStdname(String stdname) {
+        this.stdname = stdname;
+    }
+
+    /**
+     * @return the attd
+     */
+    public String getAttd() {
+        return attd;
+    }
+
+    /**
+     * @param attd the attd to set
+     */
+    public void setAttd(String attd) {
+        this.attd = attd;
+    }
+    /**
+     * @return the sTime
+     */
+    
+    private String stime;
+    private String etime;
+    private String subject;
+    private String stdname;
+    private String attd;
+    
+    
+
+}
